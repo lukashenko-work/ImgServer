206172332f51e517001f561072798b13ee185d86
lukashenko-work <lukashenko.work@mail.ru>
2026-01-16 00:35:10 +0300

    feat: images upload

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def format_file_size(size_bytes):
 
 def generate_unique_filename(origina_filename):
     ext = get_file_extension(origina_filename)
-    unique_id = str(uuid.uui4())
+    unique_id = str(uuid.uuid4())
     return f'{unique_id}{ext}'
 
 def validate_file(filename, file_size):
@@ -107,6 +107,58 @@ def validate_file(filename, file_size):
 
     return True, None
 
+#Прасинг multipart/form-data
+def parse_multipart_form_data(content_type, body):
+    """
+    Парсит multipart/form-data и извлекает файл
+    Возвращает (filename, file_content) или (None, None) при ошибке
+    :param content_type: Description
+    :param body: Description
+    """
+    try:
+        if 'boundary=' not in content_type:
+            return None, None
+        
+        boundary = content_type.split('boundary=')[1].strip()
+        boundary_bytes = f'--{boundary}'.encode()
+
+        parts = body.split(boundary_bytes)
+        for part in parts:
+            if b'Content-Disposition' in part:
+                disposition_line = part.split(b'\r\n')[1].decode('utf-8')
+                if 'filename' in disposition_line:
+                    #Парсим имя файла
+                    filename_start = disposition_line.find('filename="') + 10
+                    filename_end = disposition_line.find('"', filename_start)
+                    filename = disposition_line[filename_start:filename_end]
+
+                    #Извлекаем содержимое файла
+                    content_start = part.find(b'\r\n\r\n') + 4
+                    content_end = part.rfind(b'\r\n')
+                    file_content = part[content_start:content_end]
+
+                    return filename, file_content
+        return None, None
+    except Exception as e:
+        log_error(f'Ошибка парсинга multipart {e}')
+        return None, None
+
+def save_file(filename, file_content):
+    try:
+        new_filname = generate_unique_filename(filename)
+        file_path = os.path.join(IMAGE_DIR, new_filname)
+
+        with open(file_path, 'wb') as f:
+            f.write(file_content)
+        
+        log_success(f'Файл {new_filname} (оригинал: {filename}) сохранен')
+        return True, new_filname
+    except Exception as e:
+        error_msg = f'Ошибка сохранения файла: {e}'
+        log_error(error_msg)
+        return False, error_msg
+
+
 class ImageServerHandler(BaseHTTPRequestHandler):
     """Обработчик HTTP запросов для сервера изображений"""
 
@@ -116,7 +168,68 @@ class ImageServerHandler(BaseHTTPRequestHandler):
             self.log('Запрос главной страницы')
             self.send_welcome_page()
         else:
-            self.send_error_response(404, 'Маршрут не найден')
+            self.send_error_response(404, f'Маршрут {self.path} не найден')
+
+    def do_POST(self):
+        if self.path == '/upload':
+            self.handle_upload()
+        else:
+            self.send_error_response(404, f'Маршрут {self.path} не найден')
+
+    def handle_upload(self):
+        try:
+            content_type = self.headers.get('Content-Type', '')
+            content_length = int(self.headers.get('Content-Length', 0))
+
+            if content_length > MAX_FILE_SIZE:
+                self.log(f'Файл слишком большой: {format_file_size(content_length)}', 'error')
+                self.send_error_response(
+                    413,
+                    f'Файл слишком большой. Максимум: {format_file_size(MAX_FILE_SIZE)}'
+                )
+                return
+            len
+            body = self.rfile.read(content_length)
+            
+            filename, file_content = parse_multipart_form_data(content_type, body)
+
+            if not filename or not file_content:
+                self.log('Не удалось извлечь файл из запроса', 'error')
+                self.send_error_response(400, 'Файл не найден в запросе')
+                return
+
+            is_valid, error_message = validate_file(filename, len(file_content))
+            if not is_valid:
+                self.log(f'Файл не прошел валидацию {error_message}')
+                self.send_error_response(400, error_message)
+                return
+            
+            success, result = save_file(filename, file_content)
+
+            if success:
+                new_filename = result
+                file_url = f'{IMAGE_DIR}/{new_filename}'
+
+                self.log(f'Файл загружен: {new_filename}')
+
+                response_data = {
+                    'success': True,
+                    'message': 'Файл успешно загружен',
+                    'filename': new_filename,
+                    'original_filename': filename,
+                    'size': format_file_size(len(file_content)),
+                    'url': file_url
+                }
+                self.send_json_response(200, response_data)
+            else:
+                error_msg = result
+                self.log(f'Ошибка загрузки: {error_msg}')
+                self.send_error_response(500, error_msg)
+        except Exception as e:
+            error_msg = f'Непредвиденная ошибка: {e}'
+            self.log(error_msg, 'error')
+            self.send_error_response(500, error_msg)
+
 
     def send_welcome_page(self):
         """Отправляет приветственное сообщение"""
